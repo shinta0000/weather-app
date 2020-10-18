dadba230a03d2da20da755e1d9a78a7f19d66868
ueno <uenoshinta0k@gmail.com>
2020-10-18 11:11:01 +0900

    取得したidによって天気のアイコンを振り分ける処理を追加

--- src/App.js
+++ src/App.js
@@ -38,6 +38,34 @@ class App extends React.Component {
     return cell;
   }
 
+  get_WeatherIcon(icons, rangeId) {
+    switch(true) {
+      case rangeId >= 200 && rangeId <= 232:
+        this.setState({icon: this.weatherIcon.Thunderstorm})
+        break;
+      case rangeId >= 300 && rangeId <= 321:
+        this.setState({icon: this.weatherIcon.Drizzle})
+        break;
+      case rangeId >= 500 && rangeId <= 531:
+        this.setState({icon: this.weatherIcon.Rain})
+        break;
+      case rangeId >= 600 && rangeId <= 622:
+        this.setState({icon: this.weatherIcon.Snow})
+        break;
+      case rangeId >= 701 && rangeId <= 781:
+        this.setState({icon: this.weatherIcon.Atmosphere})
+        break;
+      case rangeId === 800:
+        this.setState({icon: this.weatherIcon.Clear})
+        break;
+      case rangeId >= 801 && rangeId <= 804:
+        this.setState({icon: this.weatherIcon.Clouds})
+        break;
+      default:
+        this.setState({icon: this.weatherIcon.Clouds})
+    }
+  }
+
   // fetchするデータにhttp://の記述をしたらうまく行った
   getWeather = async () => {
     const api_call = await fetch(
@@ -55,8 +83,10 @@ class App extends React.Component {
       temp_max: this.calCelsius(response.main.temp_max),
       temp_min: this.calCelsius(response.main.temp_min),
       description: response.weather[0].description,
-      icon: this.weatherIcon.Snow,
+      // icon: this.weatherIcon.Snow,
     });
+
+    this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
   };
 
   render() {
